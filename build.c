dfc2688955a98371719ac57ae512122c036f1118
lua4web <lua4web@gmail.com>
2013-06-23 18:17:22 +0400

    added __prepare call

--- build.c
+++ build.c
@@ -5,6 +5,29 @@
 static int build(lua_State *L) {
 	// 1 - self
 
+	int self = luaL_ref(L, LUA_REGISTRYINDEX);
+	lua_rawgeti(L, LUA_REGISTRYINDEX, self); // might need this
+
+	lua_getfield(L, 1, "__prepare");
+	// 1 - self
+	// 2 - __prepare
+
+	if(lua_isfunction(L, 2)) {
+		lua_rawgeti(L, LUA_REGISTRYINDEX, self);
+		// 1 - self
+		// 2 - __prepare
+		// 3 - self
+
+		lua_call(L, 1, 0);
+	}
+	else {
+		lua_pop(L, 1);
+	}
+
+	luaL_unref(L, LUA_REGISTRYINDEX, self); 
+
+	// 1 - self
+
 	lua_getfield(L, 1, "__template");
 	// 1 - self
 	// 2 - template
@@ -15,6 +38,10 @@ static int build(lua_State *L) {
 		s = "";
 		len = 0;
 	}
+	
+	lua_pop(L, 1);
+
+	// 1 - self
 
 	luaL_Buffer B;
 	luaL_buffinit(L, &B);
@@ -28,15 +55,13 @@ static int build(lua_State *L) {
 				if(i) { // marker is not empty?
 					lua_pushlstring(L, s, i);
 					// 1 - self
-					// 2 - template
-					// 3 - marker
+					// 2 - marker
 
 					lua_gettable(L, 1); // pops the string!
 					// 1 - self
-					// 2 - template
-					// 3 - marker's value
+					// 2 - marker's value
 
-					if(lua_isnil(L, 3)) { // could not find marker
+					if(lua_isnil(L, 2)) { // could not find marker
 						lua_pop(L, 1); // remove nil
 						luaL_addstring(&B, "%");
 						luaL_addlstring(&B, s, i); // add "%"..marker.."%"
